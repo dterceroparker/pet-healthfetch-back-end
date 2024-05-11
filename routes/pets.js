67c7f65edc84c7e710e8851eecacc16453c6f227
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-05-10 21:13:36 -0400

    add deleteVisit routes and controller function

--- routes/pets.js
+++ routes/pets.js
@@ -15,5 +15,6 @@ router.get('/:petId', checkAuth, petsCtrl.show)
 router.put('/:petId', checkAuth, petsCtrl.update)
 router.post('/:petId/visits', checkAuth, petsCtrl.createVisit)
 router.put('/:petId/visits', checkAuth, petsCtrl.updateVisit)
+router.delete('/:petId/visits/:visitId', checkAuth, petsCtrl.deleteVisit)
 
 export { router }
